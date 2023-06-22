8d83a6ae6a9d68d03b098261ad2e79eb54258e89
Peter Phillips <phillipspc@gmail.com>
2023-06-22 09:13:09 -0400

    require/run setupdb

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "dotenv"
 require "json"
 require "roda"
+require_relative "setupdb"
 require_relative "handle_slash_command_request_job"
 
 Dotenv.load
--- setupdb.rb
+++ setupdb.rb
@@ -2,8 +2,6 @@ require "sequel"
 
 DB = Sequel.sqlite('./nowplaying.db')
 
-DB.drop_table? :users
-
 DB.create_table? :users do
   primary_key :id
   column :slack_id, String, { unique: true }
